52c9b1884a7d29a18012e3b6945708fc170f2da1
Moataz <motazosama1111@gmail.com>
2023-03-17 12:15:54 +0200

    'commit3'

--- js/index.js
+++ js/index.js
@@ -1,6 +1,8 @@
 let inputSearch = document.querySelector("#search");
 let tWeather = document.querySelector("#tableWeather");
-// current day
+async function getWeather(x = "Alexandria"){
+    // current day
+let cDay =document.querySelector("#currentDay");
 let cDate =document.querySelector("#currentDate");
 let myCity = document.querySelector("#city");
 let cNum = document.querySelector("#num");
@@ -8,47 +10,53 @@ let cImage = document.querySelector("#currentImage");
 let cCondation = document.querySelector("#currentCondition");
 // *****************
 // second day
-let sDate =document.querySelector("#secondDate");
+let sDay =document.querySelector("#secondDay");
 let sImage = document.querySelector("#secondImage");
 let sMaxTemp = document.querySelector("#secondMaxTemp");
 let sMinTemp = document.querySelector("#secondMinTemp");
 let sConditon = document.querySelector("#secondCondition");
 // *****************
 // third day
-let thDate =document.querySelector("#thirdDate");
+let thDay =document.querySelector("#thirdDay");
 let thImage = document.querySelector("#thirdImage");
 let thMaxTemp = document.querySelector("#thirdMaxTemp");
 let thMinTemp = document.querySelector("#thirdMinTemp");
 let thConditon = document.querySelector("#thirdCondition");
 // *****************
-
-async function getWeather(x){
+// date
+let days = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
+let months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
+let d =new Date;
+let day = d.getDay();
+let month = d.getMonth();
+let date = d.getDate();
+// *******************
     let myResponse = await fetch(`https://api.weatherapi.com/v1/forecast.json?key=6abb82e761b04ddaa9652431232102&q=${x}&days=3`);
     let final =await myResponse.json();
     // current day
-    cDate.textContent=`${final.forecast.forecastday[0].date}`;
+    cDay.textContent=`${days[day]}`;
+    cDate.textContent=`${date+months[month]}`;
     myCity.textContent=`${final.location.name}`;
     cNum.innerHTML=`${final.current.temp_c} <span class="num-degree">o</span>C`;
     cImage.setAttribute("src",`${final.forecast.forecastday[1].day.condition.icon}`);
     cCondation.textContent=`${final.forecast.forecastday[1].day.condition.text}`;
     // *******************
     // second day
-    sDate.textContent=`${final.forecast.forecastday[1].date}`;
+    sDay.textContent=`${days[day + 1]}`;
     sImage.setAttribute("src",`${final.forecast.forecastday[1].day.condition.icon}`);
     sMaxTemp.innerHTML=`${final.forecast.forecastday[1].day.maxtemp_c} <span class="num-degree">o</span>C`;
     sMinTemp.innerHTML=`${final.forecast.forecastday[1].day.mintemp_c} <span class="num-degree">o</span>C`;
     sConditon.textContent=`${final.forecast.forecastday[1].day.condition.text}`;
     // ********************
     // third day
-    thDate.textContent=`${final.forecast.forecastday[2].date}`;
+    thDay.textContent=`${days[day + 2]}`;
     thImage.setAttribute("src",`${final.forecast.forecastday[2].day.condition.icon}`);
     thMaxTemp.innerHTML=`${final.forecast.forecastday[2].day.maxtemp_c} <span class="num-degree">o</span>C`;
     thMinTemp.innerHTML=`${final.forecast.forecastday[2].day.mintemp_c} <span class="num-degree">o</span>C`;
     thConditon.textContent=`${final.forecast.forecastday[2].day.condition.text}`;
 }
-
+getWeather()
 inputSearch.addEventListener("input",function(){
-    tWeather.classList.remove("d-none")
     let country = inputSearch.value;
     getWeather(country)
 });
